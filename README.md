e7d332763cca3686b736d8b8dfa1d4483c0d2ee3
Felipe Gabriel Reimer <reimermngabriel@gmail.com>
2023-02-17 10:32:23 -0300

    Criação da main e função --freq N

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # indexer
-indexer - indexa palavras de documentos de texto
+
+    Indexa palavras de documentos de texto
+
+SINOPSE
+    ./indexer --freq N ARQUIVO
+    ./indexer --freq-word PALAVRA ARQUIVO
+    ./indexer --search TERMO ARQUIVO [ARQUIVO ...]
+
+DESCRIÇÃO
+    O programa **indexer** realiza uma contagem de palavras em documentos de 
+    texto. A partir dessa contagem, ele ainda permite uma busca pelo número de 
+    ocorrências de uma palavra específica em um documento, ou a seleção de 
+    documentos relevantes para um dado termo de busca.
+    O programa **indexer** transforma todas as letras para minúsculas e ignora
+    caracteres como números e pontuações.
+    Quando executado com a opção --freq, o programa **indexer** irá exibir o 
+    número de ocorrências das N palavras mais frequentes no documento passado 
+    como parâmetro, em ordem decrescente de ocorrências.
+    Quando executado com a opção --freq-word, o programa **indexer** exibe a 
+    contagem de uma palavra específica no documento passado como parâmetro.
+    Por fim, quando executado com a opção --search, o programa **indexer** 
+    apresenta uma listagem dos documentos mais relevantes para um dado termo de 
+    busca. Para tanto, o programa utiliza o cálculo TF-IDF (Term 
+    Frequency-Inverse Document Frequency).
+
+OPÇÕES
+  --freq N ARQUIVO
+    Exibe o número de ocorrência das N palavras que mais aparecem em ARQUIVO, em
+    ordem decrescente de ocorrência.
+  --freq-word PALAVRA ARQUIVO
+    Exibe o número de ocorrências de PALAVRA em ARQUIVO. 
+  --search TERMO ARQUIVO [ARQUIVO ...]
+    Exibe uma listagem dos ARQUIVOS mais relevantes encontrados pela busca por 
+    TERMO. A listagem é apresentada em ordem descrescente de relevância. 
+    TERMO pode conter mais de uma palavra. Neste caso, deve ser indicado entre 
+    àspas.
+
---
+++ indexer.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_WORD_LEN 50
+#define MAX_NUM_WORDS 100000
+
+typedef struct word_count {
+    char word[MAX_WORD_LEN];
+    int count;
+} word_count;
+
+typedef struct hash_table {
+    word_count *table[MAX_NUM_WORDS];
+    int num_words;
+} hash_table;
+
+void freq_function(FILE *file, int n);
+void freq_word_function(FILE *file, char *word);
+void search_function(FILE *file, char *term);
+void add_word(hash_table *ht, char *word);
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Modo de uso: ./program <funcao> <complemento> <arquivo1> [<arquivo2> ...]\n");
+        return 1;
+    }
+//------------------------------------------Recebe argumentos da main-----------------------------------------------//
+
+    char *function = argv[1];
+    char *complement = argv[2];
+
+    int freq_n;
+    char *freq_word;
+    char *search_term;
+
+    if (strcmp(function, "--freq") == 0) {
+        freq_n = atoi(complement);
+    } else if (strcmp(function, "--freq-word") == 0) {
+        freq_word = complement;
+    } else if (strcmp(function, "--search") == 0) {
+        search_term = complement;
+    } else {
+        printf("Erro: funcao invalida '%s'\n", function);
+        return 1;
+    }
+
+    for (int i = 3; i < argc; i++) {
+        char *filename = argv[i];
+        FILE *file = fopen(filename, "r");
+        if (!file) {
+            printf("Erro: nao foi possivel abrir o arquivo: '%s'\n", filename);
+            continue;
+        }
+//------------------------------------------------------------------------------------------------------------------//
+
+        if (strcmp(function, "--freq") == 0) {
+            printf("Frequencia das %d palavras mais comuns em %s:\n", freq_n, argv[i]);
+            freq_function(file, freq_n);
+        } else if (strcmp(function, "--freq-word") == 0) {
+            printf("Frequencia da palavra \"%s\" em %s:\n", freq_word, argv[i]);
+            freq_word_function(file, freq_word);
+        } else if (strcmp(function, "--search") == 0) {
+            printf("O termo \"%s\" aparece nas seguintes linhas de %s:\n", search_term, argv[i]);
+            search_function(file, search_term);
+        }
+
+        fclose(file);
+    }
+
+    return 0;
+}
+//=====================================================FUNCOES=======================================================//
+
+void freq_function(FILE *file, int n) {
+    // inicializa a tabela hash
+    hash_table ht;
+    ht.num_words = 0;
+
+    // lê cada palavra do arquivo e adiciona à tabela hash
+    char word[MAX_WORD_LEN];
+    while (fscanf(file, "%s", word) != EOF) {
+        add_word(&ht, word);
+    }
+
+    // ordena a tabela hash em ordem decrescente de ocorrência
+    for (int i = 0; i < ht.num_words - 1; i++) {
+        for (int j = i + 1; j < ht.num_words; j++) {
+            if (ht.table[i]->count < ht.table[j]->count) {
+                word_count *temp = ht.table[i];
+                ht.table[i] = ht.table[j];
+                ht.table[j] = temp;
+            }
+        }
+    }
+}
+
+void freq_word_function(FILE *file, char *word) {
+    // Implementação da função de frequência de palavra com parâmetro word
+}
+
+void search_function(FILE *file, char *term) {
+    // Implementação da função de busca com parâmetro term
+}
+
+void add_word(hash_table *ht, char *word) {
+    // procura pela palavra na tabela hash
+    int index = -1;
+    for (int i = 0; i < ht->num_words; i++) {
+        if (strcmp(ht->table[i]->word, word) == 0) {
+            index = i;
+            break;
+        }
+    }
+
+    // se a palavra já existe na tabela, incrementa a contagem
+    if (index >= 0) {
+        ht->table[index]->count++;
+    }
+    // caso contrário, adiciona a palavra à tabela
+    else {
+        word_count *wc = malloc(sizeof(word_count));
+        strncpy(wc->word, word, MAX_WORD_LEN);
+        wc->count = 1;
+        ht->table[ht->num_words] = wc;
+        ht->num_words++;
+    }
+}
